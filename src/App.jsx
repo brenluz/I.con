54105230f283597c73fc516630e8c1d4e1d3fc3c
Breno <brenluz@outlook.com>
2024-05-06 17:20:33 -0300

    fixed a few errors

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-
-  return (
-    <div className="App">ola</div>
-  )
-}
+const App = () => {
+	return (
+		<>
+		</>
+	);
+};
 
 export default App

dbfc286683a81224f4a4bc716a200db045359d6f
Breno <brenluz@outlook.com>
2024-05-06 18:34:37 -0300

    Comments: added some helpful comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const App = () => {
+const App = () => { // Para rodar o app Va ao terminal e digite npm run dev
 	return (
 		<>
+			{/* Seus componentes vem aqui */}
 		</>
 	);
 };

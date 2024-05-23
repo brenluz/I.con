0eab50073f020b42e880601b94b8bb6e3cf3b89b
Ricardo <ricardosouzamatos77@gmail.com>
2024-05-23 19:19:45 -0300

    add: fiz texto2,email,info

--- src/contato-page/encontato.jsx
+++ src/contato-page/encontato.jsx
@@ -2,10 +2,12 @@ import styles from './contato.module.css'
 const Entre_contato = () => {
     return (
         <div className= {styles.background}>
-        <div className= {styles.texto1}>Entre em Contato</div>
 
-        <div className= {styles.texto2}> Estamos abertos para dialogar e desenvolver pesquisa e projetos em parceria!</div>
-        <div classname= {styles.email}>icon@ufba.br</div>
+            <h2 className= {styles.texto1}>Entre em Contato</h2>
+            <p className= {styles.texto2}>Estamos abertos para dialogar e desenvolver pesquisa e projetos em parceria!</p>
+            <p className= {styles.email}>icon@ufba.br</p>
+            <p className= {styles.info}></p>
+
         </div>
   
     );

738184d2e327cdb8068e190f8a48c3df14482907
Ricardo <ricardosouzamatos77@gmail.com>
2024-05-23 19:05:12 -0300

    novas dependências

--- src/contato-page/encontato.jsx
+++ src/contato-page/encontato.jsx
@@ -2,7 +2,10 @@ import styles from './contato.module.css'
 const Entre_contato = () => {
     return (
         <div className= {styles.background}>
-        <div className= {styles.texto}>Entre em Contato</div>
+        <div className= {styles.texto1}>Entre em Contato</div>
+
+        <div className= {styles.texto2}> Estamos abertos para dialogar e desenvolver pesquisa e projetos em parceria!</div>
+        <div classname= {styles.email}>icon@ufba.br</div>
         </div>
   
     );

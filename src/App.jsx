16ec5cc34b94c09c63861cfc2614d75bba930e12
Breno <brenluz@outlook.com>
2024-05-23 18:20:43 -0300

    add: aovivo page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Aovivo from './pages/Aovivo-page/Aovivo';
 
 const App = () => { // Para rodar o app Va ao terminal e digite npm run dev
 	return (
-		<>
-			{/* Seus componentes vem aqui */}
-		</>
+		<Aovivo />
 	);
 };
 

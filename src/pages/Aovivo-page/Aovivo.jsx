04523ff8a5775565b227e683dffa3f3b9a77fd5f
Breno <brenluz@outlook.com>
2024-05-23 19:09:05 -0300

    fix: merge errors

---
+++ src/pages/Aovivo-page/Aovivo.jsx
@@ -0,0 +1,10 @@
+const Aovivo = () => {
+    return (
+        <div>
+            <h1>AO VIVO</h1>
+            <p>Esta é a página de transmissão ao vivo</p>
+        </div>
+    );
+}
+
+export default Aovivo;

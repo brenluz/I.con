e537540b72f5dd44e44e47367a9401f8c33c9a7d
Breno <brenluz@outlook.com>
2024-05-23 19:05:02 -0300

    fix: fixed merge errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import './App.css'
-import QuemSomos from "./pages/equipe-page/equipe";
+import React from 'react'
+import './App.css' 
+import QuemSomos from './pages/equipe-page/equipe'
 
-const App = () => {
-
-  return (
-    <QuemSomos />
-  )
-}
+const App = () => { // Para rodar o app Va ao terminal e digite npm run dev
+	return (
+		<QuemSomos/>
+	);
+};
 
 export default App

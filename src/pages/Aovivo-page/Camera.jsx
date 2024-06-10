44f1c2faf8a85430fae27b47c58f3b41f30bc047
Breno <brenluz@outlook.com>
2024-06-10 20:58:38 -0300

    wave component v2

---
+++ src/pages/Aovivo-page/Camera.jsx
@@ -0,0 +1,15 @@
+const Camera = () => {
+  return(
+     <iframe 
+        width="560"
+        height="315" 
+        src="https://www.youtube.com/embed/1JYgjYdJ9Z4"
+        title="YouTube video player" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin" 
+        allowFullScreen>
+      </iframe>
+  );
+}
+
+export default Camera;

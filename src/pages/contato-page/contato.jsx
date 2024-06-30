58c5a1137715468d1e3fde5d70d95ad4824e073a
Breno <brenluz@outlook.com>
2024-06-29 22:38:48 -0300

    fix: fixed tailwind not working

--- src/pages/contato-page/contato.jsx
+++ src/pages/contato-page/contato.jsx
@@ -1,7 +1,7 @@
 import EntreContato from "./encontato.jsx";
 import styles from "./contato.module.css";
 import Enviemsg from "./enviemsg.jsx";
-import Waves from "../Components/Waves.jsx";
+import Waves from "../../Components/Waves.jsx";
 
 const Contato = () => {
 	return (

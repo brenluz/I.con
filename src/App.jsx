1d915f21451fc008daa0d8e389d58c29b0f01ff6
Breno <brenluz@outlook.com>
2024-08-16 10:40:13 -0300

    fix Typo

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import About from "./pages/about/Index.jsx";
 import AoVivo from "./pages/aovivo/Index.jsx";
 import Contato from "./pages/contato/Index.jsx";
 import QuemSomos from "./pages/equipe/Index.jsx";
-import Ainteresse from "./pages/Ainteresse/index.jsx";
+import Ainteresse from "./pages/Ainteresse/Index.jsx";
 
 const App = () => {
 	// Para rodar o app Va ao terminal e digite npm run dev

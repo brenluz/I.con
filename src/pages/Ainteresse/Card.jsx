81e6ee5f492b15fc055b8fdcc99fcb57cea7a317
Ricardo <ricardosouzamatos77@gmail.com>
2024-08-17 18:25:49 -0300

    area de intersse reorganizada

--- src/pages/Ainteresse/Card.jsx
+++ src/pages/Ainteresse/Card.jsx
@@ -1,14 +1,18 @@
 import * as PropTypes from "prop-types";
 import {Component} from "react";
+import React from "react";
 
 class Card extends Component {
     render() {
         const {icon, title, text} = this.props;
+        const modifiedIcon = React.isValidElement(icon) 
+            ? React.cloneElement(icon, { size: 25 }) // Change 50 to the desired size
+            : icon;
         return (
-            <div className="flex flex-col justify-center items-center">
-                <span className=""> {icon} </span>
-                <p>{title}</p>
-                <p>{text}</p>
+            <div className="flex flex-col justify-center items-center m-2 h-full">
+                <span> {modifiedIcon} </span>
+                <p className= "text-center font-bold">{title}</p>
+                <p className= "text-justify m-1 p-3 text-xs">{text}</p>
             </div>
         )
     }

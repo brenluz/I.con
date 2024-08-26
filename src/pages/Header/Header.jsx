362a31b9e29d2be4a34d1fd1e23e5cee6c378637
Breno <brenluz@outlook.com>
2024-08-26 14:19:48 -0300

    fix: made the header fit into the page

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -4,16 +4,17 @@ import IconLogo from "../../assets/images/logo-b.png"
 
 const Header = () => {
     return (
-        <div className="bg-primary flex items-center justify-between py-6 px-12 border-b-4">
-            <img src={IconLogo} alt="logo" />
-            <div className="text-white flex gap-7 items-center justify-center font-medium">
-                <a href="#about" className="text-main font-medium">HOME</a>
+        <div className="bg-primary flex items-center justify-between py-6 px-12 border-b-4 text-xs">
+            <img src={IconLogo} alt="logo" className="hidden sm:flex" />
+            <div className="text-white flex  items-center justify-center font-medium md:gap-7 lg:gap-7 sm:gap-x-2">
+                <a href="#home" className="hover:text-main font-medium">HOME</a>
+                <a href="#about" className="hover:text-main font-medium">SOBRE</a>
                 <a href="#equipe" className="hover:text-main font-medium">EQUIPE</a>
                 <a href="#interesse" className="hover:text-main font-medium ">ÁREAS DE INTERESSE</a>
                 <a href="#aovivo" className="hover:text-main font-medium">AO VIVO</a>
                 <a href="#contato" className="hover:text-main font-medium">CONTATO</a>
                 <a href="https://www.instagram.com/lab.icon/" className="hover:text-main">
-                    <FaInstagram className="text-3xl"/>
+                    <FaInstagram className="text-3xl md:text-md"/>
                 </a>
             </div>
         </div>
--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -2,14 +2,13 @@ import { LuChevronDownCircle } from "react-icons/lu";
 
  const Hero = () => {
     return (
-        <section className="bg-primary w-full h-hero flex items-center justify-center md:text-sm">
-            <div className="flex flex-col items-center justify-center gap-9">
+        <section className="bg-primary h-full flex items-center justify-center md:text-sm">
+            <div id="home" className="flex flex-col items-center justify-center gap-9 py-6">
                 <div className="text-7xl text-main font-bold">
-                    <span className="text-white">Lab</span>oratorio de <br/>
-                    <span className="text-white">I</span>nteratividade, <br/>
-                    <span className="text-white">CO</span>mputação e  <br/>
-                    <span className="text-white">N</span>ovas Interfaces
-            
+                    <p><span className="text-white">Lab</span>oratorio de</p>
+                    <p><span className="text-white">I</span>nteratividade,</p>
+                    <p><span className="text-white">CO</span>mputação e</p>
+                    <p><span className="text-white">N</span>ovas Interfaces</p>
                 </div>
                 <p className="text-white text-2xl text-center">
                     Estamos voltados para a <span className="font-bold">pesquisa, desenvolvimento e inovação</span> em  
@@ -17,7 +16,7 @@ import { LuChevronDownCircle } from "react-icons/lu";
                     aplicadas, sobretudo, ao desenvolvimento de novas interfaces e 
                     sistemas interativos envolvendo Inteligência Artificial.
                 </p>
-                <a href="#about" className="text-white text-2xl gap-y-3 flex items-center hover:text-[#FEC400]">
+                <a href="#about" className="text-white text-2xl gap-y-3 flex items-center hover:text-main">
                     <LuChevronDownCircle className="text-xl" /> Saiba mais
                 </a>
             </div>
